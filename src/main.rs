53fb260eb6df022d0672601e66d894cf4d4bcc22
BarzaiAtal <barzai.atal@protonmail.com>
2022-08-22 16:31:43 -0700

    Initial

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+// Input/output libraries
+use std::io; 
+// Vectors
+use std::vec;
+
+// For getting intial data
+fn initial_getter() {
+    println!("Initial Getter, standing by.\n");
+}
+
+// For calculating the differences
+fn difference_getter() {
+    println!("Difference Getter, standing by.\n");
+}
+
+fn main() {
+    // Get initial data
+    initial_getter();
+    // Calculate dollar differences
+    difference_getter();
+}
